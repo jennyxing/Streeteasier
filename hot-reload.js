8f45cb77eadd29eca5f29f8b2ae94ecf7bc8397b
Jenny X <jennyx@Jennys-MacBook-Pro.local>
2019-08-19 20:42:15 -0400

    add comments to hot reload script

--- hot-reload.js
+++ hot-reload.js
@@ -2,6 +2,7 @@
 
 const filesInDirectory = dir => new Promise (resolve =>
 
+    // https://developer.mozilla.org/en-US/docs/Web/API/FileSystemDirectoryEntry/createReader
     dir.createReader ().readEntries (entries =>
 
         Promise.all (entries.filter (e => e.name[0] !== '.').map (e =>
@@ -15,16 +16,23 @@ const filesInDirectory = dir => new Promise (resolve =>
     )
 )
 
+//concat the file names and the last modificed dates so we can detect removal/adding/renaming of files.
 const timestampForFilesInDirectory = dir =>
         filesInDirectory (dir).then (files =>
             files.map (f => f.name + f.lastModifiedDate).join ())
 
 const reload = () => {
 
+    // https://developer.chrome.com/extensions/tabs#method-query
     chrome.tabs.query ({ active: true, currentWindow: true }, tabs => { // NB: see https://github.com/xpl/crx-hotreload/issues/5
 
+        // relaod the tab
+        // https://developer.chrome.com/extensions/tabs#method-reload
         if (tabs[0]) { chrome.tabs.reload (tabs[0].id) }
 
+        //reload the app
+        // this has to happen after the tabs reloads because it stops execution of the script
+        // https://developer.chrome.com/apps/runtime#method-reload
         chrome.runtime.reload ()
     })
 }
@@ -45,10 +53,19 @@ const watchChanges = (dir, lastTimestamp) => {
 
 }
 
+// https://developer.chrome.com/extensions/management#method-getSelf
 chrome.management.getSelf (self => {
-
+    // https://developer.chrome.com/extensions/management#type-ExtensionInstallType
     if (self.installType === 'development') {
-
+        // https://developer.chrome.com/apps/runtime
+        // https://developer.chrome.com/apps/runtime#method-getPackageDirectoryEntry
+        // https://developer.chrome.com/native-client/pepper_beta/cpp/classpp_1_1_directory_entry#abd1a4a70ed1b922a232c78be62b1fe86
         chrome.runtime.getPackageDirectoryEntry (dir => watchChanges (dir))
     }
 })
+
+// concepts/libraries illustrated in this file:
+// recursion
+// promises
+// chrome apps APIs
+// File and Directory Entries API
